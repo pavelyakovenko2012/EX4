bce356376c3e6f54c3125ab3e1dd363acdc075b2
pavelyakovenko2012 <pavel.yackovencko2012@gmail.com>
2022-08-07 16:40:23 +0500

    003

---
+++ EX003/Program.cs
@@ -0,0 +1,9 @@
+﻿double Factorial(int n)
+{
+    if(n==1) return 1;
+    else return n*Factorial(n-1);
+}
+for (int i = 0; i < 40; i++)
+{
+    System.Console.WriteLine($"{i}! = {Factorial(i)}");
+}
