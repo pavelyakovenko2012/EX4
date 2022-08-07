61c6875bc3bf763ac9f18aae587e7e292aa847e5
pavelyakovenko2012 <pavel.yackovencko2012@gmail.com>
2022-08-07 16:38:56 +0500

    001

---
+++ EX001/Program.cs
@@ -0,0 +1,48 @@
+﻿// string[,] table = new string[2,5];
+// table[1,2] = "слово";
+
+
+
+
+
+
+
+// for (int rows = 0; rows < 2; rows++)
+// {
+//     for (int columns = 0; columns < 5; columns++)
+//     {
+//         System.Console.WriteLine($"-{table[rows, columns]}-");
+//     }
+// }
+
+
+
+
+
+void PrintArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+{
+    for (int j = 0; j < matr.GetLength(1); j++)
+    {
+        System.Console.Write($"{matr[i,j]} ");
+    }
+    System.Console.WriteLine();
+}
+}
+void FillArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i,j] = new Random().Next(1,10);
+        }
+    }
+}
+
+int[,] matr = new int[3,4];
+PrintArray(matr);
+FillArray(matr);
+System.Console.WriteLine();
+PrintArray(matr);
