9a77fc7ba3c3b9f0e2a762eb1130c9d1915f6e73
pavelyakovenko2012 <pavel.yackovencko2012@gmail.com>
2022-08-07 16:39:50 +0500

    002

---
+++ EX002/Program.cs
@@ -0,0 +1,27 @@
+﻿int[,] pic = new int[,];
+void PrintImage(int[,] image)
+{
+    for (int i = 0; i < image.GetLength(0); i++)
+    {
+        for (int j = 0; j < image.GetLength(1); j++)
+        {
+           if(image[i,j]==0) System.Console.Write($" ");
+           else System.Console.Write($"+");
+        }
+        System.Console.WriteLine();
+    }
+}
+void FillImage(int row, int col)
+{
+    if(pic[row,col]==0)
+    {
+        pic[row,col]=1;
+        FillImage(row-1,col);
+        FillImage(row,col-1);
+        FillImage(row+1,col);
+        FillImage(row,col+1);
+    }
+}
+PrintImage(pic);
+FillImage(13,13);
+PrintImage(pic);
